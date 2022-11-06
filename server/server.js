11599475ff71a4299365d55bb0f61979144720dd
Paolo Pedrigal <paolopedrigal@gmail.com>
2022-11-06 15:46:48 -0800

    Created a basic route for reading all the users in the database. Also, finally connected to the PostgreSQL database.

--- server/server.js
+++ server/server.js
@@ -1,22 +1,44 @@
 require("dotenv").config();
 const { json } = require("express");
 const express = require("express");
+const db = require("./db");
 const app = express()
 
 // get port number
 const port = process.env.PORT;
 
 // CREATE a user
-app.post("/api/v1/createUser", (req, res) => {
-    res.status(201).json({
-        status: "successful",
-        data: 1234567890
-    });
-})
+// app.post("/api/v1/createUser", async (req, res) => {
+//     try {
+//         // const results = await db.query("INSERT INTO Users (username, user_password, first_name, last_name, birthday)
+//         res.status(201).json({
+//             status: "successful",
+//             data: 1234567890
+//         });
+//     } catch(error) {
+//         console.log(error)
+//     }
+
+// })
 
 // CREATE a workout entry
 
 // CREATE a personal record
+
+// READ all users information
+app.get("/api/v1/readUsers", async (req, res) => {
+    try {
+        const results = await db.query("SELECT * FROM workout.Users");
+        res.status(200).json({
+            status:"successful",
+            data: {
+                results: results.rows
+            }
+        })
+    } catch(error) {
+        console.log(error);
+    }
+})
 
 // READ a user information
 
