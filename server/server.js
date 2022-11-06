a6311e4f59485e0726df978d3d13b8f5a3f5846a
Paolo Pedrigal <paolopedrigal@gmail.com>
2022-11-06 15:06:32 -0800

    Started making basic routes and to connect to PostgreSQL database

--- server/server.js
+++ server/server.js
@@ -1,9 +1,42 @@
 require("dotenv").config();
+const { json } = require("express");
 const express = require("express");
 const app = express()
 
 // get port number
 const port = process.env.PORT;
+
+// CREATE a user
+app.post("/api/v1/createUser", (req, res) => {
+    res.status(201).json({
+        status: "successful",
+        data: 1234567890
+    });
+})
+
+// CREATE a workout entry
+
+// CREATE a personal record
+
+// READ a user information
+
+// READ a workout entry
+
+// READ a personal record
+
+// READ all personal records
+
+// UPDATE a user
+
+// UPDATE a workout entry
+
+// UPDATE a personal record
+
+// DELETE a user
+
+// DELETE a workout entry
+
+// DELETE a personal record
 
 // listen to client request
 app.listen(port, () => {console.log(`Server is up and listening on ${port}`)});
